239b604c2565e478ccb4bd07937855a5f267facc
jansolote <iancalidadtnsgcc@gmail.com>
2018-09-01 15:55:37 -0500

    correcciones menu, y creacion de productos

--- config/config.js
+++ config/config.js
@@ -9,6 +9,6 @@ module.exports={
 //////////// mongodb://jansolote:Morrowind5@ds261429.mlab.com:61429/admincommerce////
 //////mongodb://localhost/adminC
 
-///db: 'mongodb://jansolote:Morrowind5@localhost:27017/thevapezonedb',
-//db_dev: 'mongodb://jansolote:Morrowind5@localhost:27017/thevapezonedb',
+///db: 'mongodb://jansolote:Morrowind5@localhost:27017/coloretedb',
+//db_dev: 'mongodb://jansolote:Morrowind5@localhost:27017/coloretedb',
 
